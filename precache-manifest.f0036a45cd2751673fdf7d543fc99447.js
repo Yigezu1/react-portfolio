35ff92f4f313332b8f68b760fd3e4ea1fa0d0b31
Yigezu Mulugeta <Yigezumb@gmail.com>
2021-01-10 23:50:45 -0500

    Updates

---
+++ precache-manifest.f0036a45cd2751673fdf7d543fc99447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021ff27436277472fdeb6d254168202b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5eb8dd1d5c891094cef8",
+    "url": "/react-portfolio/static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "88d89ad885f7985c2b52",
+    "url": "/react-portfolio/static/css/main.b704f546.chunk.css"
+  },
+  {
+    "revision": "5eb8dd1d5c891094cef8",
+    "url": "/react-portfolio/static/js/2.5c838c51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.5c838c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d89ad885f7985c2b52",
+    "url": "/react-portfolio/static/js/main.b08b8dac.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
