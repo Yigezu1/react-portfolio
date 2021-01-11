eaf6a1af998f7007df7777085ca5605371b4045c
Yigezu Mulugeta <Yigezumb@gmail.com>
2021-01-11 14:32:50 -0500

    updated resume link again

--- build/precache-manifest.9ef64fa2d5503d4865c38d29e0b550db.js
+++ build/precache-manifest.9ef64fa2d5503d4865c38d29e0b550db.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3283cc0f27463d21c05a4779b8e3edb5",
+    "revision": "2f3246dcc3d49cad2dfbea8bd5d4fc26",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.032730b7.chunk.css"
   },
   {
-    "revision": "7354492c97e86261aa70",
+    "revision": "80e214642b452564b59c",
     "url": "/react-portfolio/static/css/main.b704f546.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.5c838c51.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7354492c97e86261aa70",
-    "url": "/react-portfolio/static/js/main.1cf0feba.chunk.js"
+    "revision": "80e214642b452564b59c",
+    "url": "/react-portfolio/static/js/main.f012aae3.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
